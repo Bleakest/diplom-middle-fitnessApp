d1fd2b243b448e755e68eae5f0756e5efddf16fc
Дмитрий <suvstreet@gmail.com>
2025-11-22 16:06:19 +0300

    feat: #100 Добавить роль 'ТРЕНЕР'
    * fix: Добавить типы для jsonwebtoken.
    
    * feat: Добавить ручку с refresh токеном.
    
    * feat: #71(частично) Добавить middleware для проверки авторизации.
    
    * feat: Добавить cookie.
    
    * feat: Добавить роль TRAINER.
    
    * feat: Добавить роль TRAINER в аутентификацию.
    
    * feat: Изменить ответ backend для frontend.
    
    * refactor: Изменить endpoint для стандартизации API.
    
    * fix: При refreshToken возвращать только роль пользователя.

--- backend/types/auth.ts
+++ backend/types/auth.ts
@@ -1,22 +1,47 @@
-export interface RegisterDTO {
-	name: string
-	emailOrPhone: string
-	password: string
-	age?: number
-	weight?: number
-	height?: number
-	waist?: number
-	chest?: number
-	hips?: number
-	arm?: number
-	leg?: number
-	goal?: string
-	restrictions?: string
-	experience?: string
-	diet?: string
-	photoFront?: string
-	photoSide?: string
-	photoBack?: string
+interface RegisterBase {
+  name: string
+  emailOrPhone: string
+  password: string
+}
+
+interface ClientFields {
+  age?: number
+  weight?: number
+  height?: number
+  waist?: number
+  chest?: number
+  hips?: number
+  arm?: number
+  leg?: number
+  goal?: string
+  restrictions?: string
+  experience?: string
+  diet?: string
+  photoFront?: string
+  photoSide?: string
+  photoBack?: string
+}
+
+interface TrainerFields {
+  telegram?: string
+  whatsapp?: string
+  instagram?: string
+  bio?: string
+}
+
+
+export interface RegisterClientDTO extends RegisterBase, ClientFields {
+  role: 'CLIENT'
+}
+
+export interface RegisterTrainerDTO extends RegisterBase, TrainerFields {
+  role: 'TRAINER'
+}
+
+export type RegisterDTO = RegisterClientDTO | RegisterTrainerDTO
+
+export interface QuerystringRole {
+	role: 'CLIENT' | 'TRAINER'
 }
 
 export interface LoginDTO {
@@ -28,14 +53,12 @@ interface Token {
 	accessToken: string
 }
 
-export interface PublicUser {
-	id: string
-	name: string
-	email: string | null
-	phone: string | null
+export interface UserRole {
+	role: QuerystringRole['role']
 }
-export interface UserWithToken {
-	user: PublicUser
+
+export interface RoleWithToken {
+	user: UserRole
 	token: Token
 }
 
